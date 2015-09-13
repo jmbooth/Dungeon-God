4b400c67db5dd8dd22390cb3ad7d2902d54361f7
Johnathen <jmbooth@mtu.edu>
2015-09-13 14:17:50 -0400

    There are now world camera bounds.

--- Assets/TopDownCameraController.cs
+++ Assets/TopDownCameraController.cs
@@ -5,6 +5,11 @@ public class TopDownCameraController : MonoBehaviour {
 
 	public float scrollSpeed = .1f;
 
+	public float maxX = 20;
+	public float minX = 0;
+	public float maxZ = 20;
+	public float minZ = 0;
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -14,13 +19,13 @@ public class TopDownCameraController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetKey (KeyCode.W)) {
+		if (Input.GetKey (KeyCode.W) && this.transform.position.x < maxX) {
 			this.transform.position =  new Vector3(this.transform.position.x+scrollSpeed, this.transform.position.y, this.transform.position.z);
-		}else if(Input.GetKey (KeyCode.A)) {	
+		}else if(Input.GetKey (KeyCode.A) && this.transform.position.z < maxZ) {	
 			this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z+ scrollSpeed);
-		}else if (Input.GetKey (KeyCode.S)) {
+		}else if (Input.GetKey (KeyCode.S) && this.transform.position.x > minX) {
 			this.transform.position = new Vector3(this.transform.position.x- scrollSpeed, this.transform.position.y, this.transform.position.z);
-		}else if(Input.GetKey (KeyCode.D)) {	
+		}else if(Input.GetKey (KeyCode.D) && this.transform.position.z > minZ) {	
 			this.transform.position =  new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z- scrollSpeed);
 		}
 	}
