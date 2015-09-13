89134163dbbbda9f8b686f7b67b9e539fe766fbc
Johnathen <jmbooth@mtu.edu>
2015-09-13 14:08:34 -0400

    Allows top down movement
    When attached to a camera it will allow simple movement of the camera.
    if the camera is oriented correctly it looks like top down movement.

---
+++ Assets/TopDownCameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TopDownCameraController : MonoBehaviour {
+
+	public float scrollSpeed = .1f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.W)) {
+			this.transform.position =  new Vector3(this.transform.position.x+scrollSpeed, this.transform.position.y, this.transform.position.z);
+		}else if(Input.GetKey (KeyCode.A)) {	
+			this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z+ scrollSpeed);
+		}else if (Input.GetKey (KeyCode.S)) {
+			this.transform.position = new Vector3(this.transform.position.x- scrollSpeed, this.transform.position.y, this.transform.position.z);
+		}else if(Input.GetKey (KeyCode.D)) {	
+			this.transform.position =  new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z- scrollSpeed);
+		}
+	}
+}
