0d84b839788310da07c5d84bbc4ffc75e3e7e37e
unknown <jmbooth@titanfall.lab.huskygames.com>
2015-09-20 14:09:28 -0400

    one can now move diangally

--- Assets/TopDownCameraController.cs
+++ Assets/TopDownCameraController.cs
@@ -21,11 +21,14 @@ public class TopDownCameraController : MonoBehaviour {
 	{
 		if (Input.GetKey (KeyCode.W) && this.transform.position.x < maxX) {
 			this.transform.position =  new Vector3(this.transform.position.x+scrollSpeed, this.transform.position.y, this.transform.position.z);
-		}else if(Input.GetKey (KeyCode.A) && this.transform.position.z < maxZ) {	
+		}
+		if(Input.GetKey (KeyCode.A) && this.transform.position.z < maxZ) {	
 			this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z+ scrollSpeed);
-		}else if (Input.GetKey (KeyCode.S) && this.transform.position.x > minX) {
+		}
+		if (Input.GetKey (KeyCode.S) && this.transform.position.x > minX) {
 			this.transform.position = new Vector3(this.transform.position.x- scrollSpeed, this.transform.position.y, this.transform.position.z);
-		}else if(Input.GetKey (KeyCode.D) && this.transform.position.z > minZ) {	
+		}
+		if(Input.GetKey (KeyCode.D) && this.transform.position.z > minZ) {	
 			this.transform.position =  new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z- scrollSpeed);
 		}
 	}
