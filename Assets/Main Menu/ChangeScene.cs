fbecea9473a023ee852a46a734db77ce3811c882
unknown <kddavis@Minecraft.lab.huskygames.com>
2015-10-04 15:57:11 -0400

    Merged in Main Menu project

---
+++ Assets/Main Menu/ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    public void SwitchScene(int level)
+    {
+        Application.LoadLevel(level);
+    }
+    public void loadMap(int mapNum)
+    {
+        Application.LoadLevel(mapNum + 3);
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Assets/Main Menu/isVisable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class isVisable : MonoBehaviour {
+
+    bool canSee;
+	// Use this for initialization
+	void Start () {
+        canSee = false;
+	}
+ 
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
