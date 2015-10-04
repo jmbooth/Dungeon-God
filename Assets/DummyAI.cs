6d2b7e657859fd7b4441c24a4927b2a8bb484200
unknown <rdbaumei@bioshock.lab.huskygames.com>
2015-10-04 14:38:20 -0400

    Spike pit now detects collision and 'damages' the AI, and has a cooldown.

---
+++ Assets/DummyAI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DummyAI : MonoBehaviour {
+
+	float turnTime;
+
+	// Use this for initialization
+	void Start () {
+		turnTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		turnTime += Time.deltaTime;
+		if (turnTime < 4) {
+			transform.Translate (Vector3.forward * 0.01f); 
+		} else if (turnTime >= 4 && turnTime < 8) {
+			transform.Translate (Vector3.back * 0.01f); 
+		} else {
+			turnTime = 0;
+		}
+	}
+
+    void TakeDamage(int damage)
+    {
+        Debug.Log("Ow!, I took " + damage + " damage!");
+    }
+}
---
+++ Assets/SpikePitTrigger.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class SpikePitTrigger : MonoBehaviour {
+
+	static float cooldown = 10; // in seconds
+	static int damage = 10;
+	float sinceLastTrigger; // in seconds
+
+	// Use this for initialization
+	void Start () {
+		sinceLastTrigger = cooldown;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (sinceLastTrigger <= cooldown) {
+			sinceLastTrigger += Time.deltaTime;
+		}
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+		if (sinceLastTrigger >= cooldown) {
+		Debug.Log ("Ahaha!");
+			collider.gameObject.SendMessage ("TakeDamage", damage);
+			sinceLastTrigger = 0;
+		}
+
+    }
+}
