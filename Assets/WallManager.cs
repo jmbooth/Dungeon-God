2dddcff285b1a10dd99208156c95a7b6d1bdd7e5
unknown <rdbaumei@bioshock.lab.huskygames.com>
2015-10-21 16:16:45 -0400

    Got some basic grid functionality done.

--- Assets/WallManager.cs
+++ Assets/WallManager.cs
@@ -5,14 +5,16 @@ public class WallManager : MonoBehaviour {
 
 	GameObject[,] WallGrid = new GameObject[10,10];
 	Object WallBase;
-	static float wSize = 5;
-	static float wOffset = wSize/2;
-	int layermask = 511;
+	Object TrapBase;
+	static int wSize = 5;
+	static float wOffset = wSize / 2.0f;
+	int layermask = 256;
 
 	// Use this for initialization
 	void Start () {
 
 		WallBase = Resources.Load ("Prefabs/Wall");
+		TrapBase = Resources.Load ("Prefabs/SpikePitPrefab 1");
 
 		for (int i = 0; i < 10; i++) {
 			for (int j = 0; j < 10; j++) {
@@ -23,12 +25,14 @@ public class WallManager : MonoBehaviour {
 		}
 	}
 
-	static Vector3 WorldToGrid(Vector3 input) {
-		Vector3 output = new Vector3();
+	static int[] WorldToGrid(Vector3 input) {
+		int[] output = new int[2];
 
-		output.x = (int) (input.x - wOffset) / wSize;
-		output.z = (int) (input.z - wOffset) / wSize;
-		output.y = input.y;
+		//output[0] = (int) Mathf.Floor ((input.x - wOffset) / wSize);
+		//output[1] = (int) Mathf.Floor ((input.z - wOffset) / wSize);
+
+		output [0] = (Mathf.FloorToInt(input.x)) / wSize;
+		output [1] = (Mathf.FloorToInt(input.z)) / wSize;
 
 		return output;
 	}
@@ -47,17 +51,34 @@ public class WallManager : MonoBehaviour {
 	void Update () {
 		Ray ray;
 		RaycastHit hit;
-		Vector3 wallHit;
 
-		if (Input.GetButtonDown("Fire1")) {
+		if (Input.GetButtonDown ("Fire1")) {
 			// Creates a ray from the camera through the cursor
-			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			
+			ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
 			// If the ray hits a wall, this destroys the wall
-			if (Physics.Raycast (ray, out hit, 200, layermask) && hit.collider.name == "Wall") {
-				wallHit = WorldToGrid (hit.point);
-				Destroy (WallGrid[(int) wallHit.x, (int) wallHit.z], 0.2f);
-				WallGrid[(int) wallHit.x, (int) wallHit.z] = null;
+			if (Physics.Raycast (ray, out hit, 200, layermask)) {
+				int[] wallHit = WorldToGrid (hit.point);
+
+				Debug.Log ("Coords x: " + hit.point.x + "  z: " + hit.point.z);
+				Debug.Log ("Grid place x: " + wallHit [0] + "  z: " + wallHit [1]);
+
+				Destroy (WallGrid [wallHit [0], wallHit [1]], 0.2f);
+				WallGrid [wallHit [0], wallHit [1]] = null;
+			}
+
+		} else if (Input.GetButtonDown ("Fire2")) {
+
+			ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+			if (Physics.Raycast (ray, out hit, 200, 2048)) {
+				int[] floorHit = WorldToGrid (hit.point);
+
+				Debug.Log ("Coords x: " + hit.point.x + "  z: " + hit.point.z);
+				Debug.Log ("Grid place x: " + floorHit [0] + "  z: " + floorHit [1]);
+
+				Vector3 pos = new Vector3 ((floorHit[0] * wSize) + wOffset, 0.5f, (floorHit[1] * wSize) + wOffset);
+				Instantiate (TrapBase, pos, Quaternion.identity);
 			}
 		}
 	}
