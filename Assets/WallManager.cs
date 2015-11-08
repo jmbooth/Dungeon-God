02aa3ecfcbaef0c91bc2ab0d2b15ee776db9d243
Johnathen <jmbooth@mtu.edu>
2015-11-08 15:02:29 -0500

    Changed how things are placed on the grid - objects can't be place under each other and now can be deleted

--- Assets/WallManager.cs
+++ Assets/WallManager.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class WallManager : MonoBehaviour {
 
-	GameObject[,] WallGrid = new GameObject[10,10];
+	static int gridDim = 20;
+	GameObject[,] WallGrid = new GameObject[gridDim,gridDim];
 	Object WallBase;
 	Object TrapBase;
-	static int wSize = 5;
+	static int wSize = 2; // Grid square size
 	static float wOffset = wSize / 2.0f;
 	int layermask = 256;
 
@@ -16,13 +17,13 @@ public class WallManager : MonoBehaviour {
 		WallBase = Resources.Load ("Prefabs/Wall");
 		TrapBase = Resources.Load ("Prefabs/SpikePitPrefab 1");
 
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
+		/*for (int i = 0; i < gridDim; i++) {
+			for (int j = 0; j < gridDim; j++) {
 				Vector3 pos = new Vector3(wSize * i + wOffset, 1, wSize * j + wOffset);
 				WallGrid[i,j] = (GameObject) Instantiate(WallBase, pos, Quaternion.identity);
 				WallGrid[i,j].name = "Wall(" + i + "," + j + ")";
 			}
-		}
+		}*/
 	}
 
 	static int[] WorldToGrid(Vector3 input) {
@@ -63,8 +64,19 @@ public class WallManager : MonoBehaviour {
 				Debug.Log ("Coords x: " + hit.point.x + "  z: " + hit.point.z);
 				Debug.Log ("Grid place x: " + wallHit [0] + "  z: " + wallHit [1]);
 
-				Destroy (WallGrid [wallHit [0], wallHit [1]], 0.2f);
-				WallGrid [wallHit [0], wallHit [1]] = null;
+				//if the wall already exists then destroy it
+				if(WallGrid[wallHit [0],wallHit [1]] != null)
+				{
+					Destroy (WallGrid [wallHit [0], wallHit [1]], 0.2f);
+					WallGrid [wallHit [0], wallHit [1]] = null;
+				}else
+				{
+					//create new wall instead of destroy it
+					Vector3 pos = new Vector3 ((wallHit[0] * wSize) + wOffset, 0.5f, (wallHit[1] * wSize) + wOffset);
+					WallGrid[wallHit [0],wallHit [1]] = (GameObject)Instantiate (WallBase, pos, Quaternion.identity);
+					WallGrid[wallHit [0],wallHit [1]].name = "Wall(" + wallHit[0] + "," + wallHit[1] + ")";
+				}
+
 			}
 
 		} else if (Input.GetButtonDown ("Fire2")) {
@@ -74,11 +86,15 @@ public class WallManager : MonoBehaviour {
 			if (Physics.Raycast (ray, out hit, 200, 2048)) {
 				int[] floorHit = WorldToGrid (hit.point);
 
-				Debug.Log ("Coords x: " + hit.point.x + "  z: " + hit.point.z);
-				Debug.Log ("Grid place x: " + floorHit [0] + "  z: " + floorHit [1]);
+				//if there is already something there - don't place a trap under it
+				if(WallGrid[floorHit [0],floorHit [1]] == null)
+				{
+					Debug.Log ("Coords x: " + hit.point.x + "  z: " + hit.point.z);
+					Debug.Log ("Grid place x: " + floorHit [0] + "  z: " + floorHit [1]);
 
-				Vector3 pos = new Vector3 ((floorHit[0] * wSize) + wOffset, 0.5f, (floorHit[1] * wSize) + wOffset);
-				Instantiate (TrapBase, pos, Quaternion.identity);
+					Vector3 pos = new Vector3 ((floorHit[0] * wSize) + wOffset, 0.5f, (floorHit[1] * wSize) + wOffset);
+					WallGrid[floorHit [0],floorHit [1]] = (GameObject)Instantiate (TrapBase, pos, Quaternion.identity);
+				}
 			}
 		}
 	}
