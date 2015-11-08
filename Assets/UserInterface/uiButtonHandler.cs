44a62e05e7dacf1a9b25037a0c86d623ad996f69
unknown <mdkempf@mtu.edu>
2015-11-08 14:17:59 -0500

    added handler for buttons, cleaned up uiHandler.cs and moved uiCanvas prefab

---
+++ Assets/UserInterface/uiButtonHandler.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class uiButtonHandler : MonoBehaviour {
+	protected bool toggleFlag = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void monsterHandler(string b){
+		Debug.Log ("here");
+		Debug.Log(b+" got clicked");
+	}
+
+	public void trapHandler(string b){
+		Debug.Log ("here");
+		Debug.Log(b + " got clicked");
+	}
+
+	void SetActivePanel(string b)
+	{
+		GameObject[] buttonPanels = new GameObject[2];
+		buttonPanels[0] = GameObject.Find("TrapPanel");
+		buttonPanels[1] = GameObject.Find("MonsterPanel");
+		Debug.Log ("toggle button pressed");
+		if (!toggleFlag)
+		{
+			GameObject.Find(b).GetComponentInChildren<Text>().text= "Trap";
+			buttonPanels[1].SetActive(false);
+			buttonPanels[0].SetActive(true);
+		}
+		else 
+		{
+			GameObject.Find(b).GetComponentInChildren<Text>().text= "Monsters";
+			buttonPanels[0].SetActive(false);
+			buttonPanels[1].SetActive(true);
+		}
+		toggleFlag = !toggleFlag;
+	}
+
+}
--- Assets/UserInterface/uiHandler.cs
+++ Assets/UserInterface/uiHandler.cs
@@ -7,118 +7,110 @@ using System.Collections.Generic;
 /*
 Prefab layout
 UI Canvas-->uiPanel  -->trapPanel   --> 6 Buttons -->Text Component
-                        buttonPanel --> 6 Buttons -->Text Component
-                        trapButton
-                        monsterButton
+						buttonPanel --> 6 Buttons -->Text Component
+						trapButton
+						monsterButton
 */
-public class uiHandler : MonoBehaviour {
-    // Use this for initialization
-    private Button[] trapButtons = new Button[6];
-    private Button[] monsterButtons = new Button[6];
-    private GameObject[] buttonPanels = new GameObject[2];
-    private Button tButton;
-    private Button mButton;
-    public Transform uiPrefab;
-    private Transform monsterPrefabs;
-    private Transform trapPrefabs;
-    private bool[] mSelect = { false, false, false, false, false, false };
-    private bool[] tSelect = { false, false, false, false, false, false };
 
-    void Start () {
-        
-        Transform canvasTrans = Instantiate (uiPrefab, 
-                                new Vector3 (145, 25, 0), Quaternion.identity) as Transform;
-        // panels that contain the buttons
-        buttonPanels[0] = GameObject.Find("TrapPanel");
-        buttonPanels[1] = GameObject.Find("MonsterPanel");
-        // these buttons switch between the main button panels
-        mButton = GameObject.Find("Monsters").GetComponent<Button>();
-        tButton = GameObject.Find("Traps").GetComponent<Button>();
-        tButton.onClick.AddListener(() => { SetActivePanel(0); });
-        mButton.onClick.AddListener(() => { SetActivePanel(1); });
-        //init rest of the buttons
-        trapButtons = GameObject.Find("TrapPanel").GetComponents<Button>();
-        monsterButtons = GameObject.Find("MonsterPanel").GetComponents<Button>();
-        // set listeners
-        for (int i = 0; i < trapButtons.Length; i++)
-        {
-            trapButtons[i].onClick.AddListener(() =>
-            {
-                TrapHandler(i);
-            });
-            monsterButtons[i].onClick.AddListener(() =>
-            {
-                MonsterHandler(i);
-            });
-        }
-        //hide monster panel
-        buttonPanels[1].SetActive(false);
-    }
+public class uiHandler : MonoBehaviour {
+	// Use this for initialization
+	protected Button[] trapButtons = new Button[6];
+	protected Button[] monsterButtons = new Button[6];
+	protected GameObject[] buttonPanels = new GameObject[2];
+	protected Button tButton;
+	public Transform uiPrefab;
+	private Transform monsterPrefabs;
+	private Transform trapPrefabs;
+	private bool[] mSelect = { false, false, false, false, false, false };
+	private bool[] tSelect = { false, false, false, false, false, false };
+	protected bool toggleFlag=true;
+	protected int playerGold;
+	protected Text goldText;
+	void Start () {
+		
+		//Transform canvasTrans = Instantiate (uiPrefab, 
+		//						new Vector3 (145, 25, 0), Quaternion.identity) as Transform;
+		// panels that contain the buttons
+		buttonPanels[0] = GameObject.Find("TrapPanel");
+		buttonPanels[1] = GameObject.Find("MonsterPanel");
+		goldText = GameObject.Find("GoldText").GetComponent<Text>();
+		playerGold = 10;
+		// these buttons switch between the main button panels
+		tButton = GameObject.Find("ToggleButton").GetComponent<Button>();
+		//init rest of the buttons
+		trapButtons = buttonPanels[0].GetComponentsInChildren<Button>();
+		monsterButtons = buttonPanels[1].GetComponentsInChildren<Button>();
+		// set listeners
+
+		//hide monster panel
+		buttonPanels[1].SetActive(false);
+	}
 	
 	// Update is called once per frame
 	void Update () {
-        for(int i=0; i<monsterButtons.Length; i++)
-        {
-            /*
-            if (mSelect[i] == true)
-            {
+		//update Player gold text
+		goldText.text = "Gold: "+playerGold;
+		for(int i=0; i<monsterButtons.Length; i++)
+		{
+			/*
+			if (mSelect[i] == true)
+			{
 
-            }
-            else if () {
-                mSelect[i] = false;
-            }
-            if (tSelect[i] == true)
-            {
-               
-            }
-            else if (){
-                tSelect[i] = false;
-            }
-            */
-         }
-    }
+			}
+			else if () {
+				mSelect[i] = false;
+			}
+			if (tSelect[i] == true)
+			{
+			   
+			}
+			else if (){
+				tSelect[i] = false;
+			}
+			*/
+		 }
+	}
 
-    void TrapHandler(int t)
-    {
-        for (int i = 0; i < monsterButtons.Length; i++)
-        {
-            if (i == t && tSelect[i] == false)
-            {
-                tSelect[i] = true;
-            }
-            else
-            {
-                tSelect[i] = false;
-            }
-        }
-    }
+	void TrapHandler(int t)
+	{
+		Debug.Log (trapButtons [t].name + " pressed");
+		for (int i = 0; i < monsterButtons.Length; i++)
+		{
+			if (i == t && tSelect[i] == false)
+			{
+				tSelect[i] = true;
+			}
+			else
+			{
+				tSelect[i] = false;
+			}
+		}
+	}
 
-    void MonsterHandler(int m)
-    {
-        for(int i = 0; i < monsterButtons.Length; i++)
-        {
-            if (i == m & mSelect[i] == false)
-            {
-                mSelect[i] = true;
-            }
-            else
-            {
-                mSelect[i] = false;
-            }
-        }
-    }
 
-    void SetActivePanel(int p)
-    {
-        if (p == 0)
-        {
-            buttonPanels[1].SetActive(false);
-            buttonPanels[0].SetActive(true);
-        }
-        else if (p == 1)
-        {
-            buttonPanels[0].SetActive(false);
-            buttonPanels[1].SetActive(true);
-        }
-    }
+	void MonsterHandler(int m)
+	{
+		Debug.Log(monsterButtons[m].name + " pressed");
+		monsterButtons [m].image.color = Color.blue;
+		for(int i = 0; i < monsterButtons.Length; i++)
+		{
+			if (i == m & mSelect[i] == false)
+			{
+				mSelect[i] = true;
+			}
+			else
+			{
+				mSelect[i] = false;
+			}
+		}
+	}
+
+
+	void setPlayerGold(int gold){
+		playerGold = gold;
+	}
+
+	int getPlayerGold(){
+		return playerGold;
+	}
 }
