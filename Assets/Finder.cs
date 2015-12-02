40bde8aca0d8bf64e4c7bc4f501a26051159a5af
mtjohns1 <mtjohns1@mtu.edu>
2015-12-01 20:04:24 -0500

    added relay points to the dumb AI so that they can be used for adventurers to navigate a maze.. kinda

--- Assets/Finder.cs
+++ Assets/Finder.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class Finder : MonoBehaviour {
     public Transform target;
     public Transform target1;
+    public Transform target2;
+    public Transform target3;
+    public Transform target4;
     Vector3 destinationGoal;
     Vector3 destinationTresure;
     NavMeshAgent agent;
@@ -23,7 +26,7 @@ public class Finder : MonoBehaviour {
         destinationTresure = agent.destination;
         // Update destination if the target moves one unit
         if (goal == 0)
-            if (Vector3.Distance(agent.transform.position, target.position) > 1.0f)
+            if (Vector3.Distance(agent.transform.position, target.position) > 2.0f)
             {
 
                 destinationTresure = target.position;
@@ -31,14 +34,50 @@ public class Finder : MonoBehaviour {
                 agent.destination = destinationTresure;
             }
             else goal = 1;
-        if (goal ==1)
-            if (Vector3.Distance(destinationGoal, target1.position) > 1.0f)
+        if (goal == 1)
+        {
+            if (Vector3.Distance(agent.transform.position, target1.position) > 2.0f)
             {
 
                 destinationGoal = target1.position;
 
                 agent.destination = destinationGoal;
             }
+            else goal = 2;
+        }
+        if (goal == 2)
+        {
+            if (Vector3.Distance(agent.transform.position, target2.position) > 2.0f)
+            {
+
+                destinationGoal = target2.position;
+
+                agent.destination = destinationGoal;
+            }
+            else goal = 3;
+        }
+        if (goal == 3)
+        {
+            if (Vector3.Distance(agent.transform.position, target3.position) > 2.0f)
+            {
+
+                destinationGoal = target3.position;
+
+                agent.destination = destinationGoal;
+            }
+            else goal = 4;
+        }
+        if (goal == 4)
+        {
+            if (Vector3.Distance(agent.transform.position, target4.position) > 2.0f)
+            {
+
+                destinationGoal = target4.position;
+
+                agent.destination = destinationGoal;
+            }
+            else goal = 5;
+        }
     }
     /*
 	public Transform destinationPoint;
